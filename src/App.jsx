9050ffdcd9704c46e3e9b929bf17473b9916e806
PAN SU NADI <pansu2970@gmail.com>
2024-01-31 16:15:35 +0630

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Card from "./components/card/Card";
-import { data } from "autoprefixer";
 
 const App = () => {
   const [cardData, setCardData] = useState();
 
   useEffect(() => {
     fetch("./data.json")
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => setCardData(data));
   }, []);
 
-  cardData?.map((card) => {
-    console.log(card.company);
+  const cardElements = cardData?.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        company={card.company}
+        position={card.position}
+        role={card.role}
+        level={card.level}
+        postedAt={card.postedAt}
+        contract={card.contract}
+        location={card.location}
+        languages={card.languages}
+        logo={card.logo}
+      />
+    );
   });
-
-  return <div className="p-5 bg-bgColor font-league-spartan"></div>;
+  return (
+    <div className="p-5 bg-bgColor font-league-spartan">{cardElements}</div>
+  );
 };
 
 export default App;
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import FavIcon from "../../images/photosnap.svg";
 
 const Card = (props) => {
-  const { title, profile } = props;
+  const {
+    company,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    logo,
+  } = props;
 
   return (
     <div className="flex flex-col rounded-md p-7 shadow-2xl bg-white gap-5 mt-12">
       <div className="flex flex-col gap-4">
         <div>
-          <img src={FavIcon} alt="" className="-mt-16 w-20" />
+          <img src={logo} alt="" className="-mt-16 w-20" />
         </div>
         <div className="flex flex-col gap-2">
-          <h4 className="text-primary text-xl font-bold">{title}</h4>
-          <h3 className="text-black text-xl font-bold">{profile}</h3>
+          <h4 className="text-primary text-xl font-bold">{company}</h4>
+          <h3 className="text-black text-xl font-bold">{position}</h3>
           <ul className="flex justify-between text-gray-500 text-xl gap-5">
-            <li>2days ago</li>
-            <li className="list-disc">Part time</li>
-            <li className="list-disc">USA only</li>
+            <li>{postedAt}</li>
+            <li className="list-disc">{contract}</li>
+            <li className="list-disc">{location}</li>
           </ul>
         </div>
       </div>
@@ -25,20 +34,19 @@ const Card = (props) => {
 
       <div className="flex justify-start flex-wrap gap-3">
         <div className="bg-bgColor p-2 rounded-md text-primary font-bold text-xl">
-          react
+          {role}
         </div>
         <div className="bg-bgColor p-2 rounded-md text-primary font-bold text-xl">
-          javascript
+          {level}
         </div>
-        <div className="bg-bgColor p-2 rounded-md text-primary font-bold text-xl">
-          html
-        </div>
-        <div className="bg-bgColor p-2 rounded-md text-primary font-bold text-xl">
-          html
-        </div>
-        <div className="bg-bgColor p-2 rounded-md text-primary font-bold text-xl">
-          html
-        </div>
+        {languages?.map((language) => (
+          <div
+            key={language}
+            className="bg-bgColor p-2 rounded-md text-primary font-bold text-xl"
+          >
+            {language}
+          </div>
+        ))}
       </div>
     </div>
   );
